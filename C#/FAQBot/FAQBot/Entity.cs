fdf3808bb17b1e0bd893b4a7d94c57035383ed32
Julian Hüpppauff <julian@hueppauff.com>
2018-04-14 09:34:26 +0200

    Added Entity Class

---
+++ C#/FAQBot/FAQBot/Entity.cs
@@ -0,0 +1,15 @@
+﻿namespace FAQBot
+{
+    public class Entity
+    {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public int StartingIndex { get; set; }
+
+        public int EndingIndex { get; set; }
+
+        public float Score { get; set; }
+    }
+}
