5c0f7efcce2254cf11e8cbb578053b8e12f1b1e2
Julian Hüpppauff <julian@hueppauff.com>
2018-04-28 21:30:49 +0200

    Added knowlage adapter

---
+++ Node/FAQBot/KnowlageAdapter.js
@@ -0,0 +1,19 @@
+import http from "https";
+import request from "request";
+
+this.GetQnAAnswer = function(message, knowledgeBasesId, subscriptionKey, hostname) {
+  var options = { method: 'POST',
+    url: hostname + "/qnamaker/v1.0/knowledgebases/" + knowledgeBasesId + "/generateAnswer",
+    headers: 
+     { 'Cache-Control': 'no-cache',
+       'Ocp-Apim-Subscription-Key': subscriptionKey,
+       'Content-Type': 'application/json' },
+    body: { question: 'when did world war 2 ended' },
+    json: true };
+  
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+  
+    console.log(body);
+  });
+}
