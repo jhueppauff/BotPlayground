1506f4e25112448ce39e6e8a0709a92570711133
Julian Hüpppauff <julian@hueppauff.com>
2018-04-28 21:31:19 +0200

    added return

--- Node/FAQBot/KnowlageAdapter.js
+++ Node/FAQBot/KnowlageAdapter.js
@@ -14,6 +14,6 @@ this.GetQnAAnswer = function(message, knowledgeBasesId, subscriptionKey, hostnam
   request(options, function (error, response, body) {
     if (error) throw new Error(error);
   
-    console.log(body);
+    return(body);
   });
 }
